537b0f0728f265ab2a6a1b1f34cdc1ce84bff169
LucasTaborda99 <llucas352@gmail.com>
2022-03-07 11:26:54 -0300

    Toggle Menu

--- app.js
+++ app.js
@@ -67,3 +67,28 @@ function smoothScrollTo(endX, endY, duration) {
     window.scroll(newX, newY)
   }, 1000 / 60) // 60 fps
 }
+
+
+// BOTÃO MENU
+// Criando uma constante para o botão
+const btnMobile = document.getElementById('btn-mobile');
+
+// Criando a função toggleMenu
+function toggleMenu(event) {
+  if (event.type === 'touchstart') event.preventDefault();
+  const nav = document.getElementById('nav-menu');
+  // Adicionando uma classe caso não tenha ou removendo caso tenha (toggle), no nav-menu
+  nav.classList.toggle('active');
+  const active = nav.classList.contains('active');
+  event.currentTarget.setAttribute('aria-expanded', active);
+  if(active) { 
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
+
+// Adicionando um evento, evento de clique
+btnMobile.addEventListener('click', toggleMenu);
+btnMobile.addEventListener('touchstart', toggleMenu);
+
